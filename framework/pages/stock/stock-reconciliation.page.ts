ccf9034584b4845b5520e115487901a197517724
Trương Thị Xuân Sang <125573581+Hokage36@users.noreply.github.com>
2026-03-23 12:05:45 +0700

    Update stock reconciliation page

--- framework/pages/stock/stock-reconciliation.page.ts
+++ framework/pages/stock/stock-reconciliation.page.ts
@@ -1,5 +1,6 @@
 import { Page } from '@playwright/test';
 
+import { toNumber } from '../../utils/frappe-api';
 import { ErpDocumentPage } from '../erp-document.page';
 
 type StockReconciliationData = {
@@ -19,15 +20,30 @@ export class StockReconciliationPage extends ErpDocumentPage {
     await this.goto('/app/stock-reconciliation/new-stock-reconciliation');
   }
 
-  async createStockReconciliation(data: StockReconciliationData): Promise<void> {
+  async createStockReconciliation(data: StockReconciliationData): Promise<{ appliedQuantity: string }> {
     await this.gotoNew();
     await this.selectFieldOption('purpose', data.purpose);
     await this.fillAutocompleteField('set_warehouse', data.warehouseName);
     await this.clickGridCell('.col.grid-static-col[data-fieldname="item_code"]', 'last');
     await this.fillGridAutocompleteField('item_code', data.itemCode);
-    await this.fillGridInputField('qty', data.quantity);
-    await this.fillGridInputField('valuation_rate', data.valuationRate);
-    await this.save();
+    await this.waitForFreezeToClear(15000);
+
+    const currentQty = await this.readInlineItemGridInputValue('qty')
+      .then((value) => toNumber(value))
+      .catch(() => NaN);
+    const requestedQty = toNumber(data.quantity);
+    const appliedQuantity =
+      Number.isFinite(currentQty) && currentQty === requestedQty ? String(requestedQty + 1) : data.quantity;
+
+    await this.fillInlineItemGridInputField('qty', appliedQuantity, 'Enter');
+    await this.fillInlineItemGridInputField('valuation_rate', data.valuationRate, 'Enter');
+    await this.page.keyboard.press('Escape').catch(() => {});
+    await this.dismissMessageDialogIfPresent();
+
+    await this.saveUntilSaved(/\/app\/stock-reconciliation\/(?!new-stock-reconciliation-)/);
+    await this.dismissMessageDialogIfPresent();
     await this.assertNoMissingFieldDialog();
+
+    return { appliedQuantity };
   }
 }
