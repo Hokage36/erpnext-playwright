294c78acc848852f92888d4fdd6c1c50f2c335e9
Trương Thị Xuân Sang <125573581+Hokage36@users.noreply.github.com>
2026-03-23 12:05:28 +0700

    Update stock entry test

--- tests/stock/stock_entry.spec.ts
+++ tests/stock/stock_entry.spec.ts
@@ -1,11 +1,23 @@
 import { masterData } from '../../framework/data/master-data';
 import { test } from '../../framework/fixtures/app.fixture';
+import { expectStockEntrySubmitted } from '../../framework/utils/erpnext-assertions';
 
-test('Tao chung tu kho', async ({ stockEntryPage }) => {
+test('Tao chung tu kho', async ({ stockEntryPage, page }) => {
   await stockEntryPage.createStockEntry({
     stockEntryType: masterData.stockEntryType,
     itemCode: masterData.itemCode,
     warehouseName: masterData.warehouseName,
     quantity: masterData.defaultQuantity,
+  });
+
+  await stockEntryPage.submit();
+
+  const stockEntryName = stockEntryPage.currentDocumentName();
+  await expectStockEntrySubmitted(page, {
+    expectedQty: masterData.defaultQuantity,
+    itemCode: masterData.itemCode,
+    stockEntryName,
+    stockEntryType: masterData.stockEntryType,
+    warehouseName: masterData.warehouseName,
   });
 });
