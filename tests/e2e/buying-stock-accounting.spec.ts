eba8a3d83392fbde60d30446e151c2ed79d1724c
Trương Thị Xuân Sang <125573581+Hokage36@users.noreply.github.com>
2026-03-23 12:08:48 +0700

    Update buying stock accounting e2e test

--- tests/e2e/buying-stock-accounting.spec.ts
+++ tests/e2e/buying-stock-accounting.spec.ts
@@ -1,22 +1,51 @@
 import { masterData } from '../../framework/data/master-data';
 import { uiText } from '../../framework/data/ui-text';
 import { test } from '../../framework/fixtures/app.fixture';
+import {
+  expectPurchaseInvoiceSubmitted,
+  expectPurchaseOrderSubmitted,
+  expectPurchaseReceiptSubmitted,
+} from '../../framework/utils/erpnext-assertions';
 import { tomorrowErpDate } from '../../framework/utils/date';
 
-test('Buying-Stock-Accounting', async ({ purchaseOrderPage, documentPage }) => {
+test('Buying-Stock-Accounting', async ({ purchaseOrderPage, documentPage, page }) => {
   await purchaseOrderPage.createPurchaseOrder({
     supplierName: masterData.supplierName,
     itemCode: masterData.itemCode,
     warehouseName: masterData.warehouseName,
     quantity: masterData.defaultQuantity,
+    rate: masterData.itemRate,
     stockUom: masterData.stockUom,
     scheduleDate: tomorrowErpDate(),
+  });
+
+  const purchaseOrderName = documentPage.currentDocumentName();
+  await expectPurchaseOrderSubmitted(page, {
+    purchaseOrderName,
+    supplierName: masterData.supplierName,
   });
 
   await documentPage.openCreateMenuItem(uiText.createMenu.purchaseReceipt);
   await documentPage.saveAndSubmit();
   await documentPage.dismissMessageDialogIfPresent();
 
+  const purchaseReceiptName = documentPage.currentDocumentName();
+  await expectPurchaseReceiptSubmitted(page, {
+    expectedQty: masterData.defaultQuantity,
+    itemCode: masterData.itemCode,
+    purchaseOrderName,
+    purchaseReceiptName,
+    warehouseName: masterData.warehouseName,
+  });
+
   await documentPage.openCreateMenuItem(uiText.createMenu.purchaseInvoice);
   await documentPage.saveAndSubmit();
+
+  const purchaseInvoiceName = documentPage.currentDocumentName();
+  await expectPurchaseInvoiceSubmitted(page, {
+    itemCode: masterData.itemCode,
+    purchaseInvoiceName,
+    purchaseOrderName,
+    purchaseReceiptName,
+  });
 });
