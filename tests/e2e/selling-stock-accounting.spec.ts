7efdfbd811d222e469414608c6040843122a07d2
Trương Thị Xuân Sang <125573581+Hokage36@users.noreply.github.com>
2026-03-23 12:09:31 +0700

    Update selling stock accounting e2e test

--- tests/e2e/selling-stock-accounting.spec.ts
+++ tests/e2e/selling-stock-accounting.spec.ts
@@ -1,9 +1,14 @@
 import { buildPaymentReferenceNo, masterData } from '../../framework/data/master-data';
 import { uiText } from '../../framework/data/ui-text';
 import { test } from '../../framework/fixtures/app.fixture';
+import {
+  expectDeliveryNoteSubmitted,
+  expectSalesInvoiceSubmitted,
+  expectSalesOrderSubmitted,
+} from '../../framework/utils/erpnext-assertions';
 import { tomorrowErpDate } from '../../framework/utils/date';
 
-test('Selling-Stock-Accounting', async ({ salesOrderPage, documentPage }) => {
+test('Selling-Stock-Accounting', async ({ salesOrderPage, documentPage, page }) => {
   test.setTimeout(180000);
 
   await salesOrderPage.createSalesOrder({
@@ -11,14 +16,38 @@ test('Selling-Stock-Accounting', async ({ salesOrderPage, documentPage }) => {
     itemCode: masterData.itemCode,
     warehouseName: masterData.warehouseName,
     quantity: masterData.defaultQuantity,
+    rate: masterData.itemRate,
     deliveryDate: tomorrowErpDate(),
+  });
+
+  const salesOrderName = documentPage.currentDocumentName();
+  await expectSalesOrderSubmitted(page, {
+    customerName: masterData.customerName,
+    salesOrderName,
   });
 
   await documentPage.openCreateMenuItem(uiText.createMenu.deliveryNote);
   await documentPage.saveAndSubmit();
 
+  const deliveryNoteName = documentPage.currentDocumentName();
+  await expectDeliveryNoteSubmitted(page, {
+    deliveryNoteName,
+    expectedQty: -Number(masterData.defaultQuantity),
+    itemCode: masterData.itemCode,
+    salesOrderName,
+    warehouseName: masterData.warehouseName,
+  });
+
   await documentPage.openCreateMenuItem(uiText.createMenu.salesInvoice);
   await documentPage.saveAndSubmit();
+
+  const salesInvoiceName = documentPage.currentDocumentName();
+  await expectSalesInvoiceSubmitted(page, {
+    deliveryNoteName,
+    itemCode: masterData.itemCode,
+    salesInvoiceName,
+    salesOrderName,
+  });
 
   await documentPage.openCreateMenuItem(uiText.createMenu.payment);
   await documentPage.fillReferenceNumber(buildPaymentReferenceNo());
